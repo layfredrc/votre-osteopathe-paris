f66db12bf6fffd58c64e39adb3fa1ed3284b2ebe
layfredrc <47161258+layfredrc@users.noreply.github.com>
2021-05-01 16:34:16 +0200

    UI update Changelog

--- components/Changelog.vue
+++ components/Changelog.vue
@@ -19,7 +19,7 @@
                     <div class="md:text-right md:pr-10">
                     <span
                         class="font-bold text-gray-700 inline-block pt-1 pl-2 border-l-4 border-pale-green md:border-l-0 md:pl-0">
-                        Expériences professionnelles
+                        Parcours scolaire
                     </span>
                     <span
                         class="h-8 w-8 rounded-full bg-pale-green text-pale-green hidden md:inline-flex items-center justify-center md:absolute md:-right-4 md:ml-6">
@@ -46,9 +46,13 @@
                     </div>
 
 
-                    <div class="mt-10 space-y-10">
+                    
                         
-                        <div class="bg-white rounded-xl shadow-2xl md:flex">
+                        
+
+
+
+                        <!-- <div class="bg-white rounded-xl shadow-2xl md:flex">
                             <img src="https://images.unsplash.com/photo-1584432810601-6c7f27d2362b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80" alt="Night Sky" class="md:w-1/3 md:rounded-l-lg rounded-t-lg md:rounded-t-none">
                             <div class="p-6">
                             <h2 class="font-bold text-xl md:text-3xl mb-2 text-dark-green">Expérience en Hôpital</h2>
@@ -84,13 +88,82 @@
                             <p class="text-dark-green">Cabinet d’ostéopathie à Paris 16</p>
                             </div>
 
-                        </div>
-                       
-                    </div>
-
-                    
+                        </div> -->  
                 </div>
             </div>
+
+
+            <div class="md:grid md:grid-cols-4 max-w-screen-lg mx-auto parcours">
+                <div class="md:col-span-1 md:pr-12 lg:pr-16">
+                <div class="md:border-r h-full relative pb-4 md:pb-0 md:pt-2">
+                    <div class="md:text-right md:pr-10">
+                    <span
+                        class="font-bold text-gray-700 inline-block pt-1 pl-2 border-l-4 border-pale-green md:border-l-0 md:pl-0">
+                        Expériences professionnelles
+                    </span>
+                    <span
+                        class="h-8 w-8 rounded-full bg-pale-green text-pale-green hidden md:inline-flex items-center justify-center md:absolute md:-right-4 md:ml-6">
+                        <svg class="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke="white">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                        </svg>
+                    </span>
+                    </div>
+                </div>
+                </div>
+                <div class="md:col-span-3 border-b pb-8 mb-12">
+                    <div class="prose md:prose-lg space-y-4">
+                        <img src="https://images.unsplash.com/photo-1584432810601-6c7f27d2362b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80" alt="Placeholder image" class="rounded-lg mb-12">
+                        <h2 class="font-bold text-gray-700">Expérience en Hôpital</h2>
+                        <p>Suivi et prise en charge dans différents hôpitaux (Centre Hospitalier Intercommunale de Créteil, Hôpital de Tenon, Clinique de Vauban, Centre de Réadaptation de Coubert).</p>
+                    </div>
+                    <div class="prose md:prose-lg space-y-4 mt-12">
+                        <img src="../assets/sportif.svg" alt="Placeholder image" class="rounded-lg mb-12">
+                        <h2 class="font-bold text-gray-700">Préparatrice physique</h2>
+                        <p>Stagiaire préparatrice physique au FC Saint-Vrain</p>
+                    </div>
+                    <div class="prose md:prose-lg space-y-4 mt-12">
+                        <img src="../assets/services.svg" alt="Placeholder image" class="rounded-lg mb-12" />
+                        <h2 class="font-bold text-gray-700">Practicienne ostéopathe</h2>
+                            <p>Cabinet d’ostéopathie à Nanteuil-Les-Meaux</p>
+                            <p >Cabinet d’ostéopathie à Paris 13</p>
+                            <p>Cabinet d’ostéopathie à Paris 16</p>
+                    </div>
+                </div>
+            </div>
+
+              <div class="md:grid md:grid-cols-4 max-w-screen-lg mx-auto parcours">
+                <div class="md:col-span-1 md:pr-12 lg:pr-16">
+                <div class="md:border-r h-full relative pb-4 md:pb-0 md:pt-2">
+                    <div class="md:text-right md:pr-10">
+                    <span
+                        class="font-bold text-gray-700 inline-block pt-1 pl-2 border-l-4 border-pale-green md:border-l-0 md:pl-0">
+                        Bénévolat
+                    </span>
+                    <span
+                        class="h-8 w-8 rounded-full bg-pale-green text-pale-green hidden md:inline-flex items-center justify-center md:absolute md:-right-4 md:ml-6">
+                        <svg class="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke="white">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                        </svg>
+                    </span>
+                    </div>
+                </div>
+                </div>
+                <div class="md:col-span-3 border-b pb-8 mb-12">
+                    <div class="prose md:prose-lg space-y-4">
+                        <img src="../assets/pregnant.svg" alt="Placeholder image" class="rounded-lg mb-12">
+                        <h2 class="font-bold text-gray-700">Bénévolat pour l'UNEHEO</h2>
+                        <p>Bénévole ostéopathe au sein de l’Union Nationale des Enfants Handicapés Espoir Ostéopathique (UNEHEO)</p>
+                    </div>
+               
+                </div>
+            </div>
+
+
+
 
             <div class="md:grid md:grid-cols-4 max-w-screen-lg mx-auto parcours">
                 <div class="md:col-span-1 md:pr-12 lg:pr-16">
@@ -111,9 +184,21 @@
                     </div>
                 </div>
                 </div>
-                <div class="md:col-span-3 border-b pb-8 mb-12 ">
-                <div class="prose md:prose-lg space-y-16">
-                    <div class="bg-white rounded-xl shadow-2xl md:flex">
+                <div class="md:col-span-3 border-b pb-8 mb-12 space-y-8">
+                <div class="prose md:prose-lg space-y-4">
+                    <img src="../assets/Component 3.jpeg" alt="Placeholder image" class="rounded-lg mb-12">
+                    <h2 class="font-bold text-gray-700">Massage drainant lymphatique de la méthode certifiée Renata França</h2>
+                    <p>Ce drainage permet de ‘sécher’ et d’affiner le corps avec des résultats visibles dès la première séance. Il faudra alors compléter cette session par une pratique sportive régulière et une alimentation saine.</p>
+                   
+                </div>
+                <div class="prose md:prose-lg space-y-4">
+                    <img src="../assets/gal-8.jpeg" alt="Placeholder image" class="rounded-lg mb-12">
+                    <h2 class="font-bold text-gray-700">OstéoBébé - M. Eric Simon & Mme Dany Heintz-Blondy</h2>
+                    <p>Fondé sur la Tradition, cet enseignement permet de gérer efficacement les traitements des bébés et des enfants tant des points de vue physique, émotionnel que chimique et énergétique.</p>
+                   
+                </div>
+
+                    <!-- <div class="bg-white rounded-xl shadow-2xl md:flex">
                             <img src="../assets/massage.jpeg" alt="Night Sky" class="md:w-1/3 md:rounded-l-lg rounded-t-lg md:rounded-t-none">
                             <div class="p-6">
                             <h2 class="font-bold text-xl md:text-3xl mb-2 text-dark-green">Massage drainant lymphatique de la méthode certifiée Renata França</h2>
@@ -131,10 +216,10 @@
                          
                         </div>
 
-                    </div>
+                    </div> -->
              
 
-                </div>
+               
                 </div>
             </div>
 
